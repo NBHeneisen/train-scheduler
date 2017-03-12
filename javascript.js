60d76884bdbc1e3bb0f3a8984891da3b4787df70
Nicholas Heneisen <Nicholas Heneisen>
2017-03-12 16:22:42 -0400

    Filled out HTML, added firebase initialize script

--- javascript.js
+++ javascript.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
 
+// Initialize Firebase
+var config = {
+apiKey: "AIzaSyD0vDOfHVYjhbzrqlUozZRySnNPKulXiXU",
+authDomain: "train-scheduler-db.firebaseapp.com",
+databaseURL: "https://train-scheduler-db.firebaseio.com",
+storageBucket: "train-scheduler-db.appspot.com",
+messagingSenderId: "31849461683"
+};
+firebase.initializeApp(config);
+
+
 
 
 }); //end documet werapper
