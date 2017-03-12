3d166d522ea5e1082434eaffff6f61e557fb2631
Nicholas Heneisen <Nicholas Heneisen>
2017-03-12 18:50:17 -0400

    Fleshed out javascript created newTrain object and onclick function for adding form info to object.

--- javascript.js
+++ javascript.js
@@ -10,7 +10,35 @@ messagingSenderId: "31849461683"
 };
 firebase.initializeApp(config);
 
-moment().format();
+//create variable to use for the database
+var schedule = firebase.database();
+
+//add newTrain empty object to add to
+var newTrain ={};
+
+//adds form information to database and displays in table
+//on button click of add train
+$("form").on("click","#addTrain",function(){
+
+    //prevents default submit & page refresh
+    event.preventDefault();
+
+    //adds values to newTrain object
+    newTrain.name=$("#trainName").val().trim();
+    newTrain.destination=$("#destination").val().trim();
+    newTrain.time=$("#firstTime").val().trim();
+    newTrain.frequency=$("#frequency").val().trim();
+    newTrain.timestamp=firebase.database.ServerValue.TIMESTAMP
+    //empty form
+    $('#trainForm')[0].reset();
+
+    //console log newTrain object
+    console.log(newTrain);
+
+}); //end addTrain onclick function
 
 
-}); //end documet werapper
+
+
+
+}); //end document werapper
