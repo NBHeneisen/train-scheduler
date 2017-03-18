1ca2259fc3f9f91705f3d37d135d720e9b6559e7
Nicholas Heneisen <Nicholas Heneisen>
2017-03-18 00:17:05 -0400

    Debugged and completed equations for table

--- javascript.js
+++ javascript.js
@@ -16,6 +16,14 @@ var schedule = firebase.database();
 //add newTrain empty object to add to
 var newTrain ={};
 
+//empties table body
+$("tbody").empty();
+
+//
+schedule.ref().on("child_added", function(childSnapshot){
+	addTrain(childSnapshot.val());
+});
+
 //adds form information to database and displays in table
 //on button click of add train
 $("form").on("click","#addTrain",function(){
@@ -28,15 +36,35 @@ $("form").on("click","#addTrain",function(){
     newTrain.destination=$("#destination").val().trim();
     newTrain.time=$("#firstTime").val().trim();
     newTrain.frequency=$("#frequency").val().trim();
-    newTrain.timestamp=firebase.database.ServerValue.TIMESTAMP
+	schedule.ref().push(newTrain);
     //empty form
     $('#trainForm')[0].reset();
 
     //console log newTrain object
-    console.log(newTrain);
+    console.log(schedule);
 
-}); //end addTrain onclick function
+}); //end onclick function
 
+//function to add Train to schedule
+function addTrain(train) {
+    var first = moment(train.time, "HH:mm").subtract(1,"years");
+    console.log(first);
+    var current = moment();
+    console.log(moment().format("HH:mm"));
+    var difference = current.diff(first, "minutes");
+    console.log("diff " + difference);
+    var remainder = difference % JSON.parse(train.frequency);
+    console.log(remainder);
+    var minutesToNext = JSON.parse(train.frequency) - remainder;
+    var nextArrival = current.add(minutesToNext, "minutes").format("HH:mm");
+    $("tbody").append("<tr><td>" + 
+        train.name + "</td><td>" +
+        train.destination + "</td><td>" + 
+        train.frequency + "</td><td>" + 
+        nextArrival + "</td><td>" +
+        minutesToNext + "</td></tr>"
+    );
+}; //End addTrain
 
 
 
